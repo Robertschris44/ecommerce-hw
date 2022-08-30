800b25d3de076d330c75c1bc40f138eb3b0269c7
Chris Key <robertschris010@gmail.com>
2022-08-29 23:41:54 -0500

    starting on category

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -1,12 +1,33 @@
 const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const { Category, Product, Tag, ProductTag } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
+ // find all categories
   // be sure to include its associated Products
+router.get('/', (req, res) => {
+ 
+  Category.findAll({
+    include: {
+      model: Product,
+      attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+    }
+  })
+  .then(dbCategoryData => {
+    if(!dbCategoryData) {
+      //return message if category is not found
+      res.status(404).json({message: 'Categories not found'});
+      return;
+    }
+    res.json(dbCategoryData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err)
+  });
 });
+
+
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
