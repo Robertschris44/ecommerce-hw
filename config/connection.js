850633b304430da8e848d0bb41cab1d9c2509c56
Chris Key <robertschris010@gmail.com>
2022-09-03 13:01:32 -0500

    bcs support

--- config/connection.js
+++ config/connection.js
@@ -8,15 +8,16 @@ let sequelize;
 
 if(process.env.JAWSDB_URL){
   sequelize = new Sequelize(process.env.JAWSDB_URL);
-}
-
-else{
-  sequelize = new Sequelize(process.env.DB_USER, process.env.DB_PASSWORD, {
+}else{
+  sequelize = new Sequelize(process.env.DB_USER, process.env.DB_PASSWORD, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
-    port: 3306
-  });
+    port: 3306,
+  }
+  );
 }
+
+
 
 
 
